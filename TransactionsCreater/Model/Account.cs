6779aa28336684add8976cd46d8386f2ba989ba5
Lev <leva.100s@gmail.com>
2022-08-17 23:24:36 +0300

    Add generation date for deposit based registration date on lead

--- TransactionsCreater/Model/Account.cs
+++ TransactionsCreater/Model/Account.cs
@@ -5,7 +5,7 @@ public class Account
     public long Id { get; set; }
     public int Currency { get; set; }
     public long LeadId { get; set; }
-
+    public DateTime LeadRegistrationDate { get; set; }
 
     public Account(string line)
     {
@@ -13,5 +13,6 @@ public class Account
         Id = Int64.Parse(parts[0]);
         Currency = Int32.Parse(parts[1]);
         LeadId = Int64.Parse(parts[3]);
+        LeadRegistrationDate = DateTime.Parse(parts[5]);
     }
 }
